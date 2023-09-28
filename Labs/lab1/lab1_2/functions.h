6ff28b9ff37c7bcb99cb7ab50da60f050d2c6644
MaximusPokeZ <mzaslavtsev@gmail.com>
2023-09-28 22:35:55 +0300

    Lab #1.2 ver: 1.5

--- Labs/lab1/lab1_2/functions.h
+++ Labs/lab1/lab1_2/functions.h
@@ -19,6 +19,7 @@ long double precalculation_for_gamma;
 const long long MAX_N = 1e5;
  
 long long sieve_of_eratoshen[MAX_N + 1];
+long long prime_numbers[MAX_N + 1];
  
 void sieve()
 {
@@ -31,6 +32,19 @@ void sieve()
         for (long long j = 2;  i * j <= MAX_N; ++j)
         {
             sieve_of_eratoshen[i * j] = 1;
+        }
+    }
+}
+
+void prime_array ()
+{
+    long long n = 0;
+    for (long long i = 2; i <= MAX_N; i++)
+    {
+        if (sieve_of_eratoshen[i] == 0)
+        {
+            prime_numbers[n] = i;
+            n++;
         }
     }
 }
@@ -51,6 +65,7 @@ void print_info() {
     printf("Laboratory work #1_2\n\n");
 }
 //-----------------
+
 
 
 //Input & epsilon check
@@ -185,6 +200,7 @@ long double bpow(long double base, long long degree)
 }
 
 
+
 //for Euler number
 long double Euler_number_by_limit (long double epsilon)
 {
@@ -219,16 +235,13 @@ long double series_Euler_Number(long double epsilon)
     return e;
 }
 
-
 long double function_lnX_1(long double x) {
     return log(x) - 1;
 }
 
-// Derivative of the function ln(x) - 1
 long double dfunction_ln(long double x) {
     return 1.0 / x;
 }
-
 
 void print_E (long double epsilon)
 {
@@ -243,6 +256,8 @@ void multiplication_for_n_plus_one (long double * new_value, int n)
 {
     *new_value = *new_value * ((4.0L * n * (n + 1)) / ((2.0L * n + 1) * (2.0L * n + 1)));
 }
+
+
 
 //for Pi
 long double Pi_number_by_limit (long double epsilon)
@@ -277,16 +292,13 @@ long double series_pi_Number(long double epsilon)
     return pi;
 }
 
-
 long double function_cosx_1(long double x) {
     return cos(x) + 1;
 }
 
-// Derivative of the function cos(x) + 1
 long double dfunction_cos(long double x) {
     return -sin(x);
 }
-
 
 void print_pi (long double epsilon)
 {
@@ -296,6 +308,7 @@ void print_pi (long double epsilon)
     printf("By 'math.h' %.9f\n\n", M_PI);
 }
 //--------------------------------
+
 
 
 //for ln(2)
@@ -331,16 +344,13 @@ long double series_ln_Number(long double epsilon)
     return ln;
 }
 
-
 long double function_ex_2(long double x) {
     return expl(x) - 2;
 }
 
-// Derivative of the function cos(x) + 1
 long double dfunction_ex(long double x) {
     return expl(x);
 }
-
 
 void print_ln (long double epsilon)
 {
@@ -350,6 +360,7 @@ void print_ln (long double epsilon)
     printf("By 'math.h' %.9f\n\n", log(2));
 }
 //--------------------------------
+
 
 
 //for sqrt(2)
@@ -381,16 +392,13 @@ long double Product_sqrt_Number(long double epsilon)
     return result;
 }
 
-
 long double function_x2_2(long double x) {
     return x * x - 2;
 }
 
-// Derivative of the function cos(x) + 1
 long double dfunction_x2(long double x) {
     return 2.0L * x;
 }
-
 
 void print_sqrt (long double epsilon)
 {
@@ -400,6 +408,7 @@ void print_sqrt (long double epsilon)
     printf("By 'math.h' %.9f\n\n", sqrt(2));
 }
 //--------------------------------
+
 
 
 //for γ gamma
@@ -435,49 +444,37 @@ long double series_gamma_Number(long double epsilon)
     return gamma;
 }
 
-long long Get_next_prime (long long p)
-{
-    p++;
-    while (sieve_of_eratoshen[p] != 0)
-    {
-        p++;
-    } 
-    return p;
-}
-
 void precalculation_product (long double epsilon)
 {
     precalculation_for_gamma = 1.0L; 
     long double term = 1.0L, prev_term = 0.0L;
-    long long p = 1, t; 
+    long long p = 1, t = 0; 
     do
     {
-        p = Get_next_prime(p);
-        t = p;
+        p = prime_numbers[t];
         prev_term = term;
         term = ((long double)p - 1) / (long double)p;
         precalculation_for_gamma *= term;
         t++;
     } while (fabsl(term - prev_term) > epsilon);
+    t = p + 1;
     precalculation_for_gamma = precalculation_for_gamma * log(t);
 }
-
 
 long double function_e_gamma(long double x) {
     return expl(-x) - precalculation_for_gamma;
 }
 
-// Derivative of the function cos(x) + 1
 long double dfunction_e_gamma(long double x) {
     return -expl(-x);
 }
-
 
 void print_gamma (long double epsilon)
 {
     printf("By limit: %.9Lf\n", gamma_number_by_limit(epsilon));
     printf("By series: %.9Lf\n", series_gamma_Number(epsilon));
     sieve();
+    prime_array();
     precalculation_product(epsilon);
     printf("By e^-x = lim(ln(t)  П (p - 1) / p) && Newton's method: %.9Lf\n", newton_Method(0.0L, 1.0L, epsilon, &function_e_gamma, &dfunction_e_gamma));
     printf("By 'math.h' %.9f\n\n", CONST_EULER_GAMMA);
