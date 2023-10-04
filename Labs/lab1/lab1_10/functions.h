fbe2c16ce6cb0db269c8e3c28e73954f9ee32eb8
MaximusPokeZ <mzaslavtsev@gmail.com>
2023-10-04 19:39:33 +0300

    Lab #1.10 ver: 1.1

--- Labs/lab1/lab1_10/functions.h
+++ Labs/lab1/lab1_10/functions.h
@@ -1,28 +1,116 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define SIZE 33
+
+enum start_status_codes
+{
+    good_enter_radix,
+    not_good_radix
+};
+
+enum start_status_codes check_start_enter(int radix)
+{
+    if (radix < 2 || radix > 36)
+    {
+        return not_good_radix;
+    }
+    return good_enter_radix;
+}
 
 
+int char_to_num(char c)
+{
+    if (c >= '0' && c <= '9')
+    {
+        return c - '0';
+    }
+    else if (c >= 'A' && c <= 'Z')
+    {
+        return c - 'A' + 10;
+    }
+    return -1; 
+}
+
+enum correct_input_status_codes
+{
+    correct,
+    not_correct
+};
+
+enum transfer_from_str
+{
+    ok_transfer,
+    invalid_character
+};
+
+enum transfer_from_str ss_to_base_10(char* str, int base, long long * result) {
+    int len = strlen(str);
+    int i = len - 1;
+    long long multiplier = 1;
+    for (; i > 0; i--) {
+        int digit = char_to_num(str[i]);
+        if (digit == -1 || digit >= base) {
+            return invalid_character;
+        }
+        *result = *result + digit * multiplier;
+        multiplier *= base;
+    }
+    if (str[i] == '-')
+    {
+        *result = -(*result);
+    }
+    else
+    {
+        int digit = char_to_num(str[i]);
+        if (digit == -1 || digit >= base) {
+            return invalid_character;
+        }
+        *result = *result + digit * multiplier;
+        multiplier *= base;
+    }
+    return ok_transfer;
+}
+
+void transfer_from_int(char *str, long long number, int base)
+{
+    int r, sign = 1, len = 0;
+    if (number < 0)
+    {
+        sign = -1;
+        number = -number;
+        len++;
+    }
+    if (number != 0)
+    {
+        len += logl(number) / logl(base);
+    }
+    else
+    {
+        str[0] = '0';
+    }
 
 
-// ll integer_from_n_radix(char *string, int radix, enum error_type* error_return, char (*char_to_int)(char), bool (*is_char_correct)(char, int))
-// {
-//     ll result = 0, multiplier = 1;
-//     bool has_sign_entered = false, is_negative = false;
-//     int length = strnlen_s(string, 64);
+    char *ptr = str + len + 1;
+    *ptr = '\0';
+    ptr--;
+    while (number > 0)
+    {
+        r = number % base;
+        *ptr = (r > 9) ? r - 10 + 'A' : r + '0';
+        ptr--;
+        number /= base;
+    }
+    if (sign == -1)
+    {
+        *ptr = '-';
+    }
+}
 
-//     for(int i = length - 1; i >= 0; --i)
-//     {
-//         if (is_char_correct(string[i], radix) && !has_sign_entered)
-//         {
-//             result += char_to_int(string[i]) * multiplier;
-//             multiplier *= radix;
-//         } else if ((string[i] == '+' || string[i] == '-') && !has_sign_entered)
-//         {
-//             is_negative = string[i] == '-';
-//             has_sign_entered = true;
-//         } else
-//         {
-//             *error_return = error;
-//             return 0;
-//         }
-//     }
-//     *error_return = correct;
-//     return is_negative ? result * -1 : result;
+void print_to_radix(char *str, int base)
+{
+    printf("Your number in the %d number system: %s\n", base, str);
+}
