60a6250fe3fc983080b9c5fc57821e39502986d3
MaximusPokeZ <mzaslavtsev@gmail.com>
2023-10-14 15:11:33 +0300

    Lab #2.7 ver: 1.0

---
+++ Labs/lab2/lab2_7/lab2_7.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+
+
+double func_1(double x)
+{
+    return expf(x) + sqrt(1 + expf(2 * x)) - 2;
+}
+
+double func_2(double x)
+{
+    return log(x) - x + 1.8;
+}
+
+
+double dichotomy(double a, double b, double (*f)(double), const double eps)
+{
+    double d;
+    while (fabs(a - b) >= eps)
+    {
+        d = (a + b) / 2;
+        if (f(a) * f(d) > 0) {
+            a = d;
+        }
+        else
+        {
+            b = d;
+        }
+    }
+    return d;
+}
+
+    
+int main ()
+{
+    const double eps = 2.20e-10;
+    printf("Dichotomy: %.10f\n", dichotomy(-1, 0, &func_1, eps));
+    printf("Dichotomy check: %.15f\n", func_1(dichotomy(-1, 0, &func_1, eps)));
+  
+    printf("Dichotomy: %.10f \n", dichotomy(2, 3, &func_2, eps));
+    printf("Dichotomy check: %.15f\n", func_2(dichotomy(2, 3, &func_2, eps)));
+
+}
